635889feed4672371aa3517fa29aad51a133232c
xbearx1987 <xbearx1987@gmail.com>
2017-06-03 16:09:23 +0800

    setup footer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,11 +16,11 @@
 //= require bootstrap
 //= require_tree .
 
+/*===== 首页导览列变化特效 =====*/
 $(window).scroll(function () {
-  /*===== Welcome#index - 首页导览列变化 =====*/
-	if ($(this).scrollTop() > 125) {
-		$('#navbar').addClass('scroll_navbar')
+	if ($(this).scrollTop() > 125) {  //当画面高度至125px时触发效果
+		$('#navbar').addClass('scroll_navbar')  //navbar 增加 scroll_navbar CSS
 	} else {
-		$('#navbar').removeClass('scroll_navbar')
+		$('#navbar').removeClass('scroll_navbar') //navbar 移除 scroll_navbar CSS
 	}
 })
